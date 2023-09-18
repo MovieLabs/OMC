c00066f9befc5ea2305ca79b6fd57488f718fc4d
Matt Daw <matt@mattdaw.com>
2023-09-18 08:56:58 -0400

    Add utility script for validating all files in the Examples directory

---
+++ JSON-Schema/validateExamples.mjs
@@ -0,0 +1,24 @@
+import { readdirSync, readFileSync } from 'fs';
+import Ajv from "ajv";
+import addFormats from "ajv-formats";
+
+
+const schema = JSON.parse(readFileSync("./omc/omc-v0.1.schema.json", {encoding: "utf8"}));
+const ajv = new Ajv({strict: "log"});
+addFormats(ajv);
+const validateOMC = ajv.compile(schema);
+
+const exampleFileNames = readdirSync("./Examples");
+for (const exampleFN of exampleFileNames) {
+    console.log(exampleFN);
+    const example = JSON.parse(readFileSync(`./Examples/${exampleFN}`));
+    const valid = validateOMC(example);
+    if (!valid) {
+        console.log("INVALID:");
+        console.log(validateOMC.errors);
+    } else {
+        console.log("VALID");
+    }
+    console.log("");
+}
+
