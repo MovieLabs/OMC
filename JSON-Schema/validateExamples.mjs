b7fa99ec2b88cbf256d4fab10e9b6beb576da145
Matt Daw <matt@mattdaw.com>
2023-10-02 15:29:01 -0400

    Rename schema in validation script

--- JSON-Schema/validateExamples.mjs
+++ JSON-Schema/validateExamples.mjs
@@ -1,7 +1,7 @@
 import { readdirSync, readFileSync } from 'fs';
 import Ajv2019 from "ajv/dist/2019.js";
 
-const schema = JSON.parse(readFileSync("./omc/omc-v2.0.schema-2019.json", {encoding: "utf8"}));
+const schema = JSON.parse(readFileSync("./omc/omc-v2.0.schema.json", {encoding: "utf8"}));
 const ajv = new Ajv2019({allowUnionTypes: true, verbose: true});
 const validateOMC = ajv.compile(schema);
 
